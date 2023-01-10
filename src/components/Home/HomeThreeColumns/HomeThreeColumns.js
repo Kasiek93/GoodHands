40555e2733ba45238689ae81de8929b4450e7451
Kasiek93 <katarzyna.krzysiek@opoczta.pl>
2023-01-10 22:48:11 +0100

    HomeThreeColumns

---
+++ src/components/Home/HomeThreeColumns/HomeThreeColumns.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ColumnsBackground from '../../../assets/ColumnsBackground.png';
+
+const HomeThreeColumns = () => {
+    return (
+        <section className="columns">
+            <div className="columns-background">
+                <img src={ColumnsBackground} alt="background" className="columns-background-img"></img>
+            </div>
+            <div className="columns-everything">
+                <div className="columns-everything-first">
+                    <h2 className="columns-everything-first-h2">10</h2>
+                    <h3 className="columns-everything-first-h3">ODDANYCH WORKÓW</h3>
+                    <p className="columns-everyting-first-paragraph">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra
+                        elementuma. Aliquam erat volutpat.</p>
+                </div>
+                <div className="columns-everything-second">
+                    <h2 className="columns-everything-second-h2">5</h2>
+                    <h3 className="columns-everything-second-h3">WSPARTYCH ORGANIZACJI</h3>
+                    <p className="columns-everything-second-paragraph">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra
+                        elementuma. Aliquam erat volutpat.</p>
+                </div>
+                <div className="columns-everything-third">
+                    <h2 className="columns-everything-third-h2">7</h2>
+                    <h3 className="columns-everything-third-h3">ZORGANIZOWANY ZBIÓREK</h3>
+                    <p className="columns-everything-third-paragraph">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra
+                        elementuma. Aliquam erat volutpat.</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default HomeThreeColumns;
